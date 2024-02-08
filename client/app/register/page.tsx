13b5c05a3c1eaf14e97b2da3fb9f05d8cb841762
TetsuroKawakubo <tetsuro.riverkubo322@gmail.com>
2024-02-08 23:00:22 +0900

    use client追加でクリックイベント発火できるようにした

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,10 +1,15 @@
+'use client'
 import Link from "next/link";
 
 export default function Home() {
+  const onClickShowAlert = () => {
+    alert('たしカニ');
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h2 className="text-2xl font-bold mb-4">カニになる</h2>
+        <h2 className="text-2xl font-bold mb-4" onClick={onClickShowAlert}>カニになる</h2>
         <form className="w-full max-w-sm">
           <div className="mb-4">
             <label
