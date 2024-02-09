7e381f9020aeb40fe74b60aa04dbcaa4d6bdea02
TetsuroKawakubo <tetsuro.riverkubo322@gmail.com>
2024-02-09 22:47:53 +0900

    登録処理よろおね

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,15 +1,16 @@
-'use client'
+"use client";
 import Link from "next/link";
 
 export default function Home() {
-  const onClickShowAlert = () => {
-    alert('たしカニ');
-  }
+  const onClickSubmit = () => {
+    // TODO: register処理(https://github.com/cafe-lake/diary-crab/issues/1)
+    console.log("register処理開始");
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h2 className="text-2xl font-bold mb-4" onClick={onClickShowAlert}>カニになる</h2>
+        <h2 className="text-2xl font-bold mb-4">カニになる</h2>
         <form className="w-full max-w-sm">
           <div className="mb-4">
             <label
@@ -66,6 +67,10 @@ export default function Home() {
           <button
             type="submit"
             className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={(e) => {
+              e.preventDefault();
+              onClickSubmit();
+            }}
           >
             カニになる
           </button>
