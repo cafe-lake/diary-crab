b24da6bee5ffdbec6a9f37e2cfc66964b08e5cda
TetsuroKawakubo <tetsuro.riverkubo322@gmail.com>
2024-02-12 23:26:48 +0900

    認証できるよ！

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log('heelo')
+import express from "express";
+import type { Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT = 4000;
+
+const auth = require("./routes/auth");
+const post = require("./routes/post");
+
+app.use(express.json());
+app.use("/auth", auth);
+app.use("/posts", post);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello JWT");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
