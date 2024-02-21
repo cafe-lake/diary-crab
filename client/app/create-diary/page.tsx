4f8f16e29c5984b5ae13826fa2208c7e2f19c59a
TetsuroKawakubo <tetsuro.riverkubo322@gmail.com>
2024-02-21 22:35:43 +0900

    絵をペタペタして動かして楽しい！

--- client/app/create-diary/page.tsx
+++ client/app/create-diary/page.tsx
@@ -1,13 +1,28 @@
 "use client";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 
-const Canvas = dynamic(() => import('../../components/diary-canvas'), {
+const Canvas = dynamic(() => import("../../components/diary-canvas"), {
   ssr: false,
 });
 
 export default function Home() {
   const router = useRouter();
+  const divRef = useRef<HTMLInputElement>(null);
+  const [dimensions, setDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    if (divRef.current?.offsetHeight && divRef.current?.offsetWidth) {
+      setDimensions({
+        width: divRef.current.offsetWidth,
+        height: divRef.current.offsetHeight,
+      });
+    }
+  }, []);
 
   const onClickConfirm = () => {
     console.log("create");
@@ -17,10 +32,10 @@ export default function Home() {
   return (
     <main className="min-h-screen p-4 max-w-[800px] m-auto">
       <h2 className="text-2xl font-bold p-4 text-center">絵日記をかく</h2>
-      <div>
-        <Canvas />
+      <div className="w-full h-[40vh]" ref={divRef}>
+        <Canvas dimensions={dimensions} />
       </div>
-      <div>
+      <div className="mt-4">
         <textarea className="w-full h-24"></textarea>
       </div>
       <div className="text-center">
--- client/components/diary-canvas.tsx
+++ client/components/diary-canvas.tsx
@@ -1,15 +1,37 @@
-"use client"
+"use client";
 
-import { Stage, Layer, Circle } from 'react-konva';
+import { Stage, Layer, Image } from "react-konva";
+import useImage from "use-image";
 
-function Canvas(props: any) {
+export default function Canvas(props: any) {
+  const [crab] = useImage(
+    "https://4.bp.blogspot.com/-sdhuHWjgfCo/UYOsrFBf5RI/AAAAAAAARKs/THaabR1hDq4/s600/umi_kani.png"
+  );
+  const [grocery] = useImage(
+    "https://3.bp.blogspot.com/-6T6YOr6aUck/UdEenstrOOI/AAAAAAAAVzw/GCMNx0MKMGI/s718/yaoya.png"
+  );
   return (
-    <Stage width={300} height={300}>
+    <Stage
+      width={props.dimensions.width}
+      height={props.dimensions.height}
+      className="bg-white"
+    >
       <Layer>
-        <Circle x={200} y={100} radius={50} fill="green" />
+        <Image
+          alt="draw a picture!"
+          image={crab}
+          draggable
+          width={100}
+          height={100}
+        ></Image>
+        <Image
+          alt="draw a picture!"
+          image={grocery}
+          draggable
+          width={100}
+          height={100}
+        ></Image>
       </Layer>
     </Stage>
   );
 }
-
-export default Canvas;
