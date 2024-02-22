8223bf0c8d5f56d6d549b04e204a42edab6b650d
TetsuroKawakubo <tetsuro.riverkubo322@gmail.com>
2024-02-22 23:43:05 +0900

    多分絵日記は送信される

--- client/app/create-diary/page.tsx
+++ client/app/create-diary/page.tsx
@@ -2,6 +2,8 @@
 import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
+import Konva from "konva";
+import axios from "axios";
 
 const Canvas = dynamic(() => import("../../components/diary-canvas"), {
   ssr: false,
@@ -10,10 +12,12 @@ const Canvas = dynamic(() => import("../../components/diary-canvas"), {
 export default function Home() {
   const router = useRouter();
   const divRef = useRef<HTMLInputElement>(null);
+  const stageRef = useRef<Konva.Stage>(null);
   const [dimensions, setDimensions] = useState({
     width: 0,
     height: 0,
   });
+  const [text, setText] = useState("");
 
   useEffect(() => {
     if (divRef.current?.offsetHeight && divRef.current?.offsetWidth) {
@@ -24,19 +28,34 @@ export default function Home() {
     }
   }, []);
 
-  const onClickConfirm = () => {
-    console.log("create");
-    router.push("/mypage");
+  const onClickConfirm = async () => {
+    if (!stageRef.current) return;
+    const img = await stageRef.current.toDataURL();
+    const formData = new FormData();
+    formData.append("img", img);
+    formData.append("text", text);
+    console.log(formData.get("text"));
+    // const res = await axios.post(`http://example.com/upload`, formData, {
+    //   headers: {
+    //     "content-type": "multipart/form-data",
+    //   },
+    // });
+    // if (res) {
+    //   router.push("/mypage");
+    // }
   };
 
   return (
     <main className="min-h-screen p-4 max-w-[800px] m-auto">
       <h2 className="text-2xl font-bold p-4 text-center">絵日記をかく</h2>
       <div className="w-full h-[40vh]" ref={divRef}>
-        <Canvas dimensions={dimensions} />
+        <Canvas dimensions={dimensions} stageRef={stageRef} />
       </div>
       <div className="mt-4">
-        <textarea className="w-full h-24"></textarea>
+        <textarea
+          className="w-full h-24"
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
       </div>
       <div className="text-center">
         <button
--- client/components/diary-canvas.tsx
+++ client/components/diary-canvas.tsx
@@ -5,16 +5,19 @@ import useImage from "use-image";
 
 export default function Canvas(props: any) {
   const [crab] = useImage(
-    "https://4.bp.blogspot.com/-sdhuHWjgfCo/UYOsrFBf5RI/AAAAAAAARKs/THaabR1hDq4/s600/umi_kani.png"
+    "https://4.bp.blogspot.com/-sdhuHWjgfCo/UYOsrFBf5RI/AAAAAAAARKs/THaabR1hDq4/s600/umi_kani.png",
+    "anonymous"
   );
   const [grocery] = useImage(
-    "https://3.bp.blogspot.com/-6T6YOr6aUck/UdEenstrOOI/AAAAAAAAVzw/GCMNx0MKMGI/s718/yaoya.png"
+    "https://3.bp.blogspot.com/-6T6YOr6aUck/UdEenstrOOI/AAAAAAAAVzw/GCMNx0MKMGI/s718/yaoya.png",
+    "anonymous"
   );
   return (
     <Stage
       width={props.dimensions.width}
       height={props.dimensions.height}
       className="bg-white"
+      ref={props.stageRef}
     >
       <Layer>
         <Image
