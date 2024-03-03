dcc415d21c8235f4ba0795f1ab92dea00a02612e
TetsuroKawakubo <tetsuro.riverkubo322@gmail.com>
2024-03-03 17:04:21 +0900

    truncateはしない

--- server/prisma/commands/local/canvas-item.ts
+++ server/prisma/commands/local/canvas-item.ts
@@ -3,23 +3,35 @@ import { PrismaClient } from "@prisma/client";
 export const seedCanvasItems = async () => {
   const prisma = new PrismaClient();
   try {
-    await prisma.canvasItem.deleteMany();
-
-    await prisma.canvasItem.create({
-      data: {
-        label: "やおやさん",
-        url: "https://3.bp.blogspot.com/-6T6YOr6aUck/UdEenstrOOI/AAAAAAAAVzw/GCMNx0MKMGI/s718/yaoya.png",
-        display_order: 2,
+    const item1 = await prisma.canvasItem.findUnique({
+      where: {
+        id: 1,
       },
     });
+    if (!item1) {
+      await prisma.canvasItem.create({
+        data: {
+          label: "やおやさん",
+          url: "https://3.bp.blogspot.com/-6T6YOr6aUck/UdEenstrOOI/AAAAAAAAVzw/GCMNx0MKMGI/s718/yaoya.png",
+          display_order: 2,
+        },
+      });
+    }
 
-    await prisma.canvasItem.create({
-      data: {
-        label: "もぶかに",
-        url: "https://2.bp.blogspot.com/-R1xfN9n-n5I/VCOJqeGIJqI/AAAAAAAAm0k/6zzMpbfGWKU/s800/fish_kani_shiomaneki.png",
-        display_order: 1,
+    const item2 = await prisma.canvasItem.findUnique({
+      where: {
+        id: 2,
       },
     });
+    if (!item2) {
+      await prisma.canvasItem.create({
+        data: {
+          label: "もぶかに",
+          url: "https://2.bp.blogspot.com/-R1xfN9n-n5I/VCOJqeGIJqI/AAAAAAAAm0k/6zzMpbfGWKU/s800/fish_kani_shiomaneki.png",
+          display_order: 1,
+        },
+      });
+    }
   } catch (e) {
     throw e;
   }
--- server/prisma/commands/local/crab.ts
+++ server/prisma/commands/local/crab.ts
@@ -3,23 +3,35 @@ import { PrismaClient } from "@prisma/client";
 export const seedCrabs = async () => {
   const prisma = new PrismaClient();
   try {
-    await prisma.crab.deleteMany();
-
-    await prisma.crab.create({
-      data: {
-        species: "ふつうのカニ",
-        image_url:
-          "https://4.bp.blogspot.com/-sdhuHWjgfCo/UYOsrFBf5RI/AAAAAAAARKs/THaabR1hDq4/s600/umi_kani.png",
+    const crab1 = await prisma.crab.findUnique({
+      where: {
+        id: 1,
       },
     });
+    if (!crab1) {
+      await prisma.crab.create({
+        data: {
+          species: "ふつうのカニ",
+          image_url:
+            "https://4.bp.blogspot.com/-sdhuHWjgfCo/UYOsrFBf5RI/AAAAAAAARKs/THaabR1hDq4/s600/umi_kani.png",
+        },
+      });
+    }
 
-    await prisma.crab.create({
-      data: {
-        species: "毛ガニ",
-        image_url:
-          "https://2.bp.blogspot.com/-UWogktrnHxM/VRE4w8qq6WI/AAAAAAAAsXs/em-ppuMEGW0/s800/kani_kegani.png",
+    const crab2 = await prisma.crab.findUnique({
+      where: {
+        id: 2,
       },
     });
+    if (!crab2) {
+      await prisma.crab.create({
+        data: {
+          species: "毛ガニ",
+          image_url:
+            "https://2.bp.blogspot.com/-UWogktrnHxM/VRE4w8qq6WI/AAAAAAAAsXs/em-ppuMEGW0/s800/kani_kegani.png",
+        },
+      });
+    }
   } catch (e) {
     throw e;
   }
