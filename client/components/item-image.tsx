adcd02b1a290a5ecc3c5cb87c0f7cb3738aaba11
TetsuroKawakubo <tetsuro.riverkubo322@gmail.com>
2024-03-24 13:44:54 +0900

    投稿画面の画像サイズ大中小で変更可能

--- client/components/item-image.tsx
+++ client/components/item-image.tsx
@@ -1,17 +1,38 @@
 "use client";
+import { useState } from "react";
 import { Image } from "react-konva";
 import useImage from "use-image";
 
 const ItemImage = (props: any) => {
   const [img] = useImage(props.image, "anonymous");
+  const [width, setWidth] = useState(100);
+  const [height, setHeight] = useState(100);
+  const [sizeIndex, setSizeIndex] = useState(1);
+
+  const sizes = [50, 100, 200];
+
+  const onClickImage = () => {
+    let index = sizeIndex;
+    if (sizeIndex + 1 == sizes.length) {
+      index = 0;
+      setSizeIndex(index);
+    } else {
+      index = sizeIndex + 1;
+      setSizeIndex(index)
+    }
+    const size = sizes[index]
+    setHeight(size);
+    setWidth(size);
+  }
 
   return (
     <Image
       alt="draw a picture!"
       image={img}
       draggable
-      width={100}
-      height={100}
+      width={width}
+      height={height}
+      onClick={onClickImage}
     />
   );
 };
